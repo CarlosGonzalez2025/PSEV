7f387469a163fb07edb630ee4f57bf6b52b48d7f
Carlos Gonzalez <info@datenova.io>
2026-02-20 15:41:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9801675963-9d2a7",
+  "appId": "1:229359927294:web:87d1bfd2ef493f806c4aaf",
+  "apiKey": "AIzaSyCsKxeQOl5dS_Kahxdhm7EgXPWhrwXyNpI",
+  "authDomain": "studio-9801675963-9d2a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "229359927294"
+};
